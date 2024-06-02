ae35e1f3ae884faa4649217ef9d103815e3a501f
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-06-02 13:16:08 +0200

    Created the first screens the user will see

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Homepage TODO</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
---
+++ src/screens/SignInScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const SignInScreen = () => {
+  return (
+    <View>
+      <Text>Sign in TODO</Text>
+    </View>
+  );
+};
+
+export default SignInScreen;
