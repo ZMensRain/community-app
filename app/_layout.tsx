1ea7e4401b1538dc238b65789b26d4dd75f1f4c0
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-06-05 10:37:59 +0200

    added profile screen to stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import { View } from "react-native";
 
 const RootLayout = () => {
   return (
@@ -6,6 +7,13 @@ const RootLayout = () => {
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="auth/Auth" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="profile/[id]"
+        options={{
+          headerShown: true,
+          title: "Your profile",
+        }}
+      />
     </Stack>
   );
 };
