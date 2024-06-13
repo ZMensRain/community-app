f884c2b6f8b9f95c46910b5d1d9913f542408f45
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-06-13 17:39:17 +0200

    updated visuals and added color to tags

--- app/eventScreen.tsx
+++ app/eventScreen.tsx
@@ -1,6 +1,7 @@
-import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { Text, View, StyleSheet, ScrollView, Linking } from "react-native";
 import { CommunityEvent, testEvent } from "../src/model/event";
 import DayCard from "../src/components/DayCard";
+import { tagColors } from "../src/utils/tagColor";
 
 const EventScreen = () => {
   const event = testEvent;
@@ -11,19 +12,31 @@ const EventScreen = () => {
         showsHorizontalScrollIndicator={false}
         showsVerticalScrollIndicator={false}
       >
-        <View>
-          <Text style={styles.title}>{event.title}</Text>
-          <Text>Hosted by</Text>
+        <Text style={styles.h1}>{event.title}</Text>
+        <View style={styles.hosted}>
+          <Text
+            style={[
+              styles.h2,
+              {
+                alignSelf: "center",
+                marginEnd: 10,
+              },
+            ]}
+          >
+            Hosted by
+          </Text>
           <View style={styles.circle}></View>
         </View>
-        <Text>Type: {event.type}</Text>
-        <Text>
-          Age Limit: {event.ageRange["min"].toString()}
-          {event.ageRange["max"] != null ? ` —— ${event.ageRange["max"]}` : ""}
+        <Text style={styles.bodyFont}>Type: {event.type}</Text>
+        <Text style={[styles.ageLimit, styles.bodyFont]}>
+          Age Limit: {event.ageRange["min"].toString()}yr
+          {event.ageRange["max"] != null ? ` — ${event.ageRange["max"]}yr` : ""}
         </Text>
-        <View style={styles.description}>
-          <Text selectable={true}>{event.description}</Text>
-        </View>
+
+        <Text selectable={true} style={styles.description}>
+          {event.description}
+        </Text>
+
         <View style={{ flex: 1 }}>
           <ScrollView
             horizontal={true}
@@ -34,19 +47,24 @@ const EventScreen = () => {
               height: 300,
             }}
           >
-            {/*TODO make day component*/}
             {event.days.map((value, index) => {
               return <DayCard day={value} index={index} key={index} />;
             })}
           </ScrollView>
         </View>
 
-        <Text>Dress code: {event.dressCode}</Text>
+        <Text style={[styles.dressCode, styles.bodyFont]}>
+          Dress code: {event.dressCode}
+        </Text>
         <View>
-          <Text style={styles.title}>Bring</Text>
+          <Text style={styles.h1}>Bring</Text>
           <View>
             {event.kit.map((value) => {
-              return <Text>•{value}</Text>;
+              return (
+                <Text style={[styles.bringItem, styles.bodyFont]}>
+                  • {value}
+                </Text>
+              );
             })}
           </View>
         </View>
@@ -56,6 +74,54 @@ const EventScreen = () => {
             return <Text key={index}>TODO:{id}</Text>;
           })}
         </ScrollView>
+
+        {event.links.length > 0 && (
+          <View style={styles.linkContainer}>
+            <Text style={styles.h1}>Links</Text>
+            {event.links.map((link, index) => {
+              return (
+                <Text
+                  onPress={() => Linking.openURL(link.toString())}
+                  key={index}
+                  style={[styles.linkText, styles.bodyFont]}
+                >
+                  {link}
+                </Text>
+              );
+            })}
+          </View>
+        )}
+
+        {event.tags.length > 0 && (
+          <>
+            <Text style={styles.h1}>Tags</Text>
+            <View
+              style={{
+                flexDirection: "row",
+                flexWrap: "wrap",
+                alignItems: "flex-start",
+              }}
+            >
+              {event.tags.map((tag, index) => {
+                let c = tagColors(tag);
+
+                return (
+                  <View
+                    key={index}
+                    style={{
+                      backgroundColor: c.background,
+                      padding: 10,
+                      margin: 5,
+                      borderRadius: 100,
+                    }}
+                  >
+                    <Text style={{ color: c.foreground }}>{tag}</Text>
+                  </View>
+                );
+              })}
+            </View>
+          </>
+        )}
       </ScrollView>
     </View>
   );
@@ -70,14 +136,35 @@ const styles = StyleSheet.create({
   circle: {
     backgroundColor: "#aaaaaa",
     borderRadius: 100,
+    aspectRatio: 1,
+    width: "20%",
     overflow: "hidden",
   },
   description: {
-    marginVertical: 10,
+    marginVertical: 15,
+    fontSize: 16,
   },
-  title: {
+  h1: {
     fontSize: 32,
     fontWeight: "bold",
+  },
+  h2: {
+    fontSize: 25,
+    fontWeight: "500",
+  },
+  bodyFont: {
+    fontSize: 20,
+  },
+  ageLimit: {},
+  linkContainer: {
+    marginTop: 10,
+  },
+  linkText: { color: "#3a87d6" },
+  dressCode: { marginVertical: 10 },
+  bringItem: {},
+  hosted: {
+    flexDirection: "row",
+    verticalAlign: "middle",
   },
 });
 
