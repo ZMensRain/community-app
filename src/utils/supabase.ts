98cd5627e19a3375662a466fd6daca843b424dd3
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-06-14 12:20:33 +0200

    added function to get username and avatar_url from their id

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
-import 'react-native-url-polyfill/auto';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
 
 /* cSpell:disable */
 const supabaseURL = "https://yemtwnliyzhfbdclmrnn.supabase.co";
@@ -8,11 +8,25 @@ const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllbXR3bmxpeXpoZmJkY2xtcm5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5NjUyMzQsImV4cCI6MjAzMjU0MTIzNH0.N31WgDzl0WzGGhpGoTL6cXzF2kdmdDSv1XMstnQwMQ0";
 /* cSpell:enable */
 
-export const supabase = createClient(supabaseURL, supabaseKey, {
-    auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    },
-  });
+const supabase = createClient(supabaseURL, supabaseKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
+
+const getUserData = async (id: string) => {
+  let response = await supabase
+    .from("profiles")
+    .select("username, avatar_url")
+    .eq("id", id);
+  if (response.error) {
+    return response.error.message;
+  }
+
+  return response.data[0];
+};
+
+export { getUserData, supabase };
