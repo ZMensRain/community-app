5c90b0404dbbab244a1fb619f679cf2d567e9287
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-07-06 12:23:58 +0200

    created a location picker modal sheet, location picking with a map to come

---
+++ src/components/create/LocationModal.tsx
@@ -0,0 +1,27 @@
+import { BottomSheetView } from "@gorhom/bottom-sheet";
+import { Modal, StyleSheet, Text, View, Button } from "react-native";
+
+type props = {
+  onPickLocation?: (locations: { lat: number; lon: number }[]) => void;
+};
+
+/// TODO
+const LocationPickerModal = (props: props) => {
+  return (
+    <BottomSheetView style={styles.container}>
+      <Text>Unimplemented</Text>
+      <Button
+        onPress={() => {
+          props.onPickLocation?.([{ lat: 0, lon: 0 }]);
+        }}
+        title={"Set to 0, 0 for now"}
+      ></Button>
+    </BottomSheetView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 10 },
+});
+
+export default LocationPickerModal;
