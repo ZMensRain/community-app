9dc2e1cea4c85184b3f7ea2fe9e9d4a4744f69ba
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-07-07 11:07:56 +0200

    added a close button to the first screen of the event creation

--- app/create/event/_layout.tsx
+++ app/create/event/_layout.tsx
@@ -1,7 +1,11 @@
-import { Stack } from "expo-router";
+import { Stack, router } from "expo-router";
 import { CommunityEvent, DressCode } from "../../../src/model/event";
 import { useState } from "react";
 import { EventCreationContext } from "../../../src/contexts/eventCreationContext";
+
+import { faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { Pressable } from "react-native";
 
 const EventCreationLayout = () => {
   let [event, setEvent] = useState(
@@ -26,7 +30,14 @@ const EventCreationLayout = () => {
       <Stack screenOptions={{ headerBackTitle: "", title: "" }}>
         <Stack.Screen
           name="1"
-          options={{ headerBackVisible: true }}
+          options={{
+            headerBackVisible: false,
+            headerLeft: () => (
+              <Pressable onPress={() => router.back()}>
+                <FontAwesomeIcon icon={faXmarkCircle} size={24} />
+              </Pressable>
+            ),
+          }}
         ></Stack.Screen>
         <Stack.Screen name="2"></Stack.Screen>
       </Stack>
