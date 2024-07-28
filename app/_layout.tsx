39cf78a7c36f333e4c1e89f879837d5ef1e78676
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-07-28 14:28:50 +0200

    moved around the screen options

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,34 +2,23 @@ import { Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <Stack
+      screenOptions={{
+        headerBackTitleVisible: false,
+        title: "",
+        headerBackButtonMenuEnabled: false,
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="create/entry" options={{ headerShown: true }} />
+      <Stack.Screen name="index" />
+      <Stack.Screen name="auth/signIn" />
+      <Stack.Screen name="auth/signUp" />
+      <Stack.Screen name="profile/[id]" options={{ headerShown: true }} />
       <Stack.Screen
-        name="create/entry"
-        options={{
-          headerShown: true,
-          title: "",
-          headerBackTitleVisible: false,
-        }}
-      />
-      <Stack.Screen
-        name="create/event"
-        options={{
-          headerShown: false,
-          title: "",
-
-          headerBackTitleVisible: false,
-        }}
-      />
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="auth/signIn" options={{ headerShown: false }} />
-      <Stack.Screen name="auth/signUp" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="profile/[id]"
-        options={{
-          headerShown: true,
-          title: "Your profile",
-        }}
+        name="profile/YourProfile"
+        options={{ headerShown: true, title: "Your Profile" }}
       />
     </Stack>
   );
--- app/eventScreen.tsx
+++ app/eventScreen.tsx
@@ -1,158 +1,169 @@
 import { Text, View, StyleSheet, ScrollView, Linking } from "react-native";
 import { testEvent } from "src/model/event";
 import DayCard from "src/components/DayCard";
-import { tagColors } from "src/utils/stylingValue";
+import { pageStyle, tagColors } from "src/utils/stylingValue";
 import ProfileIcon from "src/components/shared/ProfileIcon";
+import { Stack } from "expo-router";
 
 const EventScreen = () => {
   const event = testEvent;
 
   return (
-    <View style={styles.container}>
-      <ScrollView
-        showsHorizontalScrollIndicator={false}
-        showsVerticalScrollIndicator={false}
-      >
-        <Text style={styles.h1}>{event.title}</Text>
-        <View style={styles.hosted}>
-          <Text
-            style={[
-              styles.h2,
-              {
-                alignSelf: "center",
-                marginEnd: 10,
-              },
-            ]}
-          >
-            Hosted by
+    <>
+      <Stack.Screen
+        options={{
+          title: event.title,
+          headerShown: true,
+        }}
+      />
+      <View style={pageStyle}>
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={styles.hosted}>
+            <Text
+              style={[
+                styles.h2,
+                {
+                  alignSelf: "center",
+                  marginEnd: 10,
+                },
+              ]}
+            >
+              Hosted by
+            </Text>
+            <ProfileIcon id={event.hosted_by} size={50} showName={false} />
+          </View>
+
+          <Text style={styles.bodyFont}>Type: {event.type}</Text>
+
+          <Text style={[styles.bodyFont, styles.vMT]}>
+            Age Limit: {event.ageRange["min"].toString()}yr
+            {event.ageRange["max"] != null
+              ? ` — ${event.ageRange["max"]}yr`
+              : ""}
           </Text>
-          <ProfileIcon id={event.hosted_by} size={50} showName={false} />
-        </View>
 
-        <Text style={styles.bodyFont}>Type: {event.type}</Text>
+          <Text style={[styles.bodyFont]}>Dress code: {event.dressCode}</Text>
 
-        <Text style={[styles.bodyFont, styles.vMT]}>
-          Age Limit: {event.ageRange["min"].toString()}yr
-          {event.ageRange["max"] != null ? ` — ${event.ageRange["max"]}yr` : ""}
-        </Text>
+          {/*Description*/}
+          <Text selectable={true} style={[styles.description, styles.vMT]}>
+            {event.description}
+          </Text>
 
-        <Text style={[styles.bodyFont]}>Dress code: {event.dressCode}</Text>
+          {/*Day cards*/}
+          <View style={{ flex: 1 }}>
+            <ScrollView
+              horizontal={true}
+              nestedScrollEnabled={true}
+              showsHorizontalScrollIndicator={false}
+              showsVerticalScrollIndicator={false}
+              style={{
+                height: 300,
+              }}
+            >
+              {event.days.map((value, index) => {
+                return <DayCard day={value} index={index} key={index} />;
+              })}
+            </ScrollView>
+          </View>
 
-        {/*Description*/}
-        <Text selectable={true} style={[styles.description, styles.vMT]}>
-          {event.description}
-        </Text>
-
-        {/*Day cards*/}
-        <View style={{ flex: 1 }}>
-          <ScrollView
-            horizontal={true}
-            nestedScrollEnabled={true}
-            showsHorizontalScrollIndicator={false}
-            showsVerticalScrollIndicator={false}
-            style={{
-              height: 300,
-            }}
-          >
-            {event.days.map((value, index) => {
-              return <DayCard day={value} index={index} key={index} />;
-            })}
-          </ScrollView>
-        </View>
-
-        {/*Kit to bring section*/}
-        {event.kit.length > 0 && (
-          <View>
-            <Text style={styles.h1}>Bring</Text>
+          {/*Kit to bring section*/}
+          {event.kit.length > 0 && (
             <View>
-              {event.kit.map((value, index) => {
+              <Text style={styles.h1}>Bring</Text>
+              <View>
+                {event.kit.map((value, index) => {
+                  return (
+                    <Text style={[styles.bodyFont]} key={index}>
+                      • {value}
+                    </Text>
+                  );
+                })}
+              </View>
+            </View>
+          )}
+
+          {/*People and groups attending*/}
+          {event.attendingIds.length > 0 && (
+            <>
+              <Text style={styles.h1}>Attending</Text>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
+              >
+                {event.attendingIds.map((id, index) => {
+                  return (
+                    <ProfileIcon
+                      id={id}
+                      size={42}
+                      showName={true}
+                      key={index}
+                    />
+                  );
+                })}
+              </ScrollView>
+            </>
+          )}
+
+          {/*Links for event*/}
+          {event.links.length > 0 && (
+            <View style={styles.vMT}>
+              <Text style={styles.h1}>Links</Text>
+              {event.links.map((link, index) => {
                 return (
-                  <Text style={[styles.bodyFont]} key={index}>
-                    • {value}
+                  <Text
+                    onPress={() => Linking.openURL(link.toString())}
+                    key={index}
+                    style={[styles.linkText, styles.bodyFont]}
+                  >
+                    {link}
                   </Text>
                 );
               })}
             </View>
-          </View>
-        )}
+          )}
 
-        {/*People and groups attending*/}
-        {event.attendingIds.length > 0 && (
-          <>
-            <Text style={styles.h1}>Attending</Text>
-            <ScrollView
-              horizontal={true}
-              showsHorizontalScrollIndicator={false}
-              showsVerticalScrollIndicator={false}
-            >
-              {event.attendingIds.map((id, index) => {
-                return (
-                  <ProfileIcon id={id} size={42} showName={true} key={index} />
-                );
-              })}
-            </ScrollView>
-          </>
-        )}
+          {/*Event tags*/}
+          {event.tags.length > 0 && (
+            <>
+              <Text style={styles.h1}>Tags</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                  alignItems: "flex-start",
+                }}
+              >
+                {event.tags.map((tag, index) => {
+                  let c = tagColors(tag);
 
-        {/*Links for event*/}
-        {event.links.length > 0 && (
-          <View style={styles.vMT}>
-            <Text style={styles.h1}>Links</Text>
-            {event.links.map((link, index) => {
-              return (
-                <Text
-                  onPress={() => Linking.openURL(link.toString())}
-                  key={index}
-                  style={[styles.linkText, styles.bodyFont]}
-                >
-                  {link}
-                </Text>
-              );
-            })}
-          </View>
-        )}
-
-        {/*Event tags*/}
-        {event.tags.length > 0 && (
-          <>
-            <Text style={styles.h1}>Tags</Text>
-            <View
-              style={{
-                flexDirection: "row",
-                flexWrap: "wrap",
-                alignItems: "flex-start",
-              }}
-            >
-              {event.tags.map((tag, index) => {
-                let c = tagColors(tag);
-
-                return (
-                  <View
-                    key={index}
-                    style={{
-                      backgroundColor: c.background,
-                      padding: 10,
-                      margin: 5,
-                      borderRadius: 100,
-                    }}
-                  >
-                    <Text style={{ color: c.foreground }}>{tag}</Text>
-                  </View>
-                );
-              })}
-            </View>
-          </>
-        )}
-      </ScrollView>
-    </View>
+                  return (
+                    <View
+                      key={index}
+                      style={{
+                        backgroundColor: c.background,
+                        padding: 10,
+                        margin: 5,
+                        borderRadius: 100,
+                      }}
+                    >
+                      <Text style={{ color: c.foreground }}>{tag}</Text>
+                    </View>
+                  );
+                })}
+              </View>
+            </>
+          )}
+        </ScrollView>
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-  },
-
   description: {
     fontSize: 16,
   },
