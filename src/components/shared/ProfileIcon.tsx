5ad2458b6a24f64338f3f6ff84d7d83ee8a8d1c1
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-08-18 10:53:33 +0200

    updated profile icon

--- src/components/shared/ProfileIcon.tsx
+++ src/components/shared/ProfileIcon.tsx
@@ -2,27 +2,32 @@ import { Pressable, StyleSheet, View, Text, ViewStyle } from "react-native";
 
 import { Id } from "~/src/model/event";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { ReactNode } from "react";
 type props = {
   id: Id;
   size: number;
   showName: boolean;
   onPress?: () => void;
   style?: ViewStyle;
+  children?: ReactNode;
 };
 
 const ProfileIcon = (props: props) => {
   return (
-    <Pressable onPress={props.onPress} style={props.style}>
+    <Pressable
+      onPress={props.onPress}
+      style={[{ width: props.size }, props.style]}
+    >
       <View>
         {/*Profile picture or icon*/}
-        <View style={{ aspectRatio: 1 }}>
+        <View style={styles.profile}>
           <Ionicons
-            name={
-              props.id.group ? "people-circle-outline" : "person-circle-outline"
-            }
+            name={props.id.group ? "people" : "person"}
             size={props.size}
-            style={{ alignSelf: "center" }}
+            style={{}}
+            adjustsFontSizeToFit={true}
           />
+          <View style={styles.children}>{props.children}</View>
         </View>
         {/*Name*/}
         {props.showName && <Text style={{ textAlign: "center" }}>Hello</Text>}
@@ -31,6 +36,23 @@ const ProfileIcon = (props: props) => {
   );
 };
 
-const styles = StyleSheet.create({ container: {} });
+const styles = StyleSheet.create({
+  container: {},
+  profile: {
+    aspectRatio: 1,
+    borderRadius: 100,
+    borderColor: "#eeeeee",
+    borderWidth: 2,
+    overflow: "hidden",
+
+    justifyContent: "flex-end",
+    alignContent: "flex-end",
+  },
+  children: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default ProfileIcon;
