1ceed55529412d582e4fb6e2a387861301213e53
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-08-23 08:59:46 +0200

    updated day card to work with the new start and end data formats

--- src/components/DayCard.tsx
+++ src/components/DayCard.tsx
@@ -1,43 +1,42 @@
 import { View, StyleSheet, Text, Button } from "react-native";
-import { Day } from "../model/event";
+import { Day } from "../model/day";
 
 type DayCardProps = {
   day: Day;
   index: number;
 };
 
-const DayCard = (props: DayCardProps) => {
-  const address = props.day.locations;
+const DayCard = ({ day, index }: DayCardProps) => {
+  const address = day.locations;
   let isPath: Boolean = false;
   let isPast: Boolean = false;
   //TODO check if is path
-  let date = props.day.date;
+  let date = day.date;
   let now = new Date();
 
   if (
     date.getFullYear() < now.getFullYear() ||
     date.getMonth() < now.getMonth() ||
     date.getDate() < now.getDate()
-  ) {
+  )
     isPast = true;
-  }
 
   return (
     <View
       style={[styles.container, isPast && styles.pastBackground]}
-      key={props.index}
+      key={index}
     >
       <Text style={[styles.title, isPast && styles.pastText]}>
-        Day {props.index + 1}
+        Day {index + 1}
       </Text>
       <Text style={[styles.date, isPast && styles.pastText]}>
-        {props.day.date.toDateString()}
+        {day.date.toDateString()}
       </Text>
       <Text style={[isPast && styles.pastText]}>
-        Starts From: {`${props.day.start["hour"]}:${props.day.start["minute"]}`}
+        Starts From: {`${day.start["hour"]}:${day.start["minute"]}`}
       </Text>
       <Text style={[isPast && styles.pastText]}>
-        Ends At: {`${props.day.end["hour"]}:${props.day.end["minute"]}`}
+        Ends At: {`${day.end["hour"]}:${day.end["minute"]}`}
       </Text>
       {isPath === false && (
         <Text style={[isPast && styles.pastText]}>
