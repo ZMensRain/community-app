bb05825ac1909b5346e2a4422c472afb9efeaea8
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-09-02 10:22:09 +0200

    added loading screen

--- app/profile/posts/[id].tsx
+++ app/profile/posts/[id].tsx
@@ -1,6 +1,6 @@
 import { router, Stack, useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
 import EventComponent from "~/src/components/EventComponent";
 import { CommunityEvent } from "~/src/model/event";
 import { pageStyle } from "~/src/utils/stylingValue";
@@ -10,7 +10,7 @@ const Posts = () => {
   const local = useLocalSearchParams();
   const userId = local["id"];
   const [posts, setPosts] = useState<CommunityEvent[]>();
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const call = async () => {
       let id = userId as string;
@@ -25,6 +25,7 @@ const Posts = () => {
       }
 
       setPosts(await getPosts(id));
+      setLoading(false);
     };
     call();
   }, []);
@@ -37,15 +38,20 @@ const Posts = () => {
           title: userId === "me" ? "My posts" : "",
         }}
       />
-      <View style={pageStyle}>
-        <FlatList
-          data={posts}
-          renderItem={(val) => {
-            if (val.item instanceof CommunityEvent)
-              return <EventComponent event={val.item} />;
-            return <Text>PlaceHolder</Text>;
-          }}
-        />
+
+      <View style={[pageStyle, loading && { justifyContent: "center" }]}>
+        {loading ? (
+          <ActivityIndicator size="large" />
+        ) : (
+          <FlatList
+            data={posts}
+            renderItem={(val) => {
+              if (val.item instanceof CommunityEvent)
+                return <EventComponent event={val.item} />;
+              return <Text>PlaceHolder</Text>;
+            }}
+          />
+        )}
       </View>
     </>
   );
