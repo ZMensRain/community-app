5515e71dec4fa5c5ab563c18b55e0f46d1fc959b
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-09-17 10:17:24 +0200

    added map and a marker for the lat and long of the user's set location

--- app/profile/components/mapSection.tsx
+++ app/profile/components/mapSection.tsx
@@ -1,21 +1,38 @@
 import { View, Text, StyleSheet, ViewStyle } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 type props = {
   style?: ViewStyle;
+  lat: number;
+  long: number;
 };
-const MapSection = ({ style }: props) => {
+const MapSection = ({ style, lat, long }: props) => {
   return (
     <View style={style}>
       <Text style={styles.h2}>Your Area</Text>
       <View
         style={{
           aspectRatio: 1,
-          borderColor: "red",
-          borderWidth: 1,
+
           borderRadius: 10,
           overflow: "hidden",
         }}
       >
-        <Text>TODO</Text>
+        <MapView
+          style={{ width: "100%", height: "100%" }}
+          zoomEnabled={false}
+          pitchEnabled={false}
+          rotateEnabled={false}
+          showsBuildings={true}
+          scrollEnabled={false}
+          region={{
+            latitude: lat,
+            longitude: long,
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.03,
+          }}
+        >
+          <Marker coordinate={{ latitude: lat, longitude: long }} />
+        </MapView>
       </View>
     </View>
   );
