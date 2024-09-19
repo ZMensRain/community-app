0ad4e131f146d2c2bd3ff297c139268cfe5b2db2
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-09-19 09:38:23 +0200

    added userProvider to the source of the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,25 @@
 import { Stack } from "expo-router";
+import { UserProvider } from "~/src/contexts/userContext";
 
 const RootLayout = () => {
   return (
-    <Stack
-      screenOptions={{
-        headerBackTitleVisible: false,
-        title: "",
-        headerBackButtonMenuEnabled: false,
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="create/entry" options={{ headerShown: true }} />
-      <Stack.Screen name="index" />
-      <Stack.Screen name="auth/signIn" />
-      <Stack.Screen name="auth/signUp" />
-      <Stack.Screen name="profile/[id]" options={{ headerShown: true }} />
-    </Stack>
+    <UserProvider>
+      <Stack
+        screenOptions={{
+          headerBackTitleVisible: false,
+          title: "",
+          headerBackButtonMenuEnabled: false,
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="create/entry" options={{ headerShown: true }} />
+        <Stack.Screen name="index" />
+        <Stack.Screen name="auth/signIn" />
+        <Stack.Screen name="auth/signUp" />
+        <Stack.Screen name="profile/[id]" options={{ headerShown: true }} />
+      </Stack>
+    </UserProvider>
   );
 };
 
