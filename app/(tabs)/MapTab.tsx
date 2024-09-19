adf40c91a2a5fde80c4e0a2fc01a22ec88d8ade3
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-09-19 09:47:31 +0200

    added map to map tab

--- app/(tabs)/MapTab.tsx
+++ app/(tabs)/MapTab.tsx
@@ -1,7 +1,15 @@
 import { View } from "react-native";
+import MapView from "react-native-maps";
 
 function map() {
-  return <View></View>;
+  return (
+    <View>
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        showsUserLocation={true}
+      ></MapView>
+    </View>
+  );
 }
 
 export default map;
