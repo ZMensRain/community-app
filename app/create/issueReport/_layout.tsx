16149772eb63e04701adab5ccc2dc192c293a218
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-09-26 14:05:17 +0200

    added creation context and removed unneeded imports

--- app/create/issueReport/_layout.tsx
+++ app/create/issueReport/_layout.tsx
@@ -1,33 +1,47 @@
 import { Stack, router } from "expo-router";
-import { CommunityEvent, DressCode } from "../../../src/model/event";
-import { useState } from "react";
-import { EventCreationContext } from "../../../src/contexts/eventCreationContext";
-
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Pressable } from "react-native";
 import { colors } from "~/src/utils/stylingValue";
-import { supabase } from "~/src/utils/supabase";
+import { IssueReportCreationProvider } from "~/src/contexts/issueReportCreationContext";
+import { Issue } from "~/src/model/issue";
 
 const IssueCreationLayout = () => {
+  const handleSubmit = (value: Issue) => {
+    router.navigate("FeedTab");
+  };
+
   return (
-    <Stack
-      screenOptions={{
-        headerBackTitleVisible: false,
-        title: "",
-      }}
-    >
-      <Stack.Screen
-        name="1"
-        options={{
-          headerLeft: () => (
-            <Pressable onPress={() => router.back()}>
-              <Ionicons name="close" size={24} color={colors.primary} />
-            </Pressable>
-          ),
-          title: "Pick Type",
+    <IssueReportCreationProvider onSubmit={handleSubmit}>
+      <Stack
+        screenOptions={{
+          headerBackTitleVisible: false,
+          title: "",
         }}
-      />
-    </Stack>
+      >
+        <Stack.Screen
+          name="1"
+          options={{
+            headerLeft: () => (
+              <Pressable onPress={() => router.back()}>
+                <Ionicons name="close" size={24} color={colors.primary} />
+              </Pressable>
+            ),
+            title: "Pick Type",
+          }}
+        />
+        <Stack.Screen
+          name="2"
+          options={{
+            headerLeft: () => (
+              <Pressable onPress={() => router.back()}>
+                <Ionicons name="close" size={24} color={colors.primary} />
+              </Pressable>
+            ),
+            title: "Pick Type",
+          }}
+        />
+      </Stack>
+    </IssueReportCreationProvider>
   );
 };
 
