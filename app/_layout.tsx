03e6ca07a5502ade5efc98cd80e4d639c379aea6
ZMR <116457838+ZMensRain@users.noreply.github.com>
2024-10-18 11:50:01 +0200

    added providers needed for bottomModalSheets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,30 @@
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { UserProvider } from "~/src/contexts/userContext";
 
 const RootLayout = () => {
   return (
     <UserProvider>
-      <Stack
-        screenOptions={{
-          headerBackTitleVisible: false,
-          title: "",
-          headerBackButtonMenuEnabled: false,
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="create/entry" options={{ headerShown: true }} />
-        <Stack.Screen name="index" />
-        <Stack.Screen name="auth/signIn" />
-        <Stack.Screen name="auth/signUp" />
-        <Stack.Screen name="profile/[id]" options={{ headerShown: true }} />
-      </Stack>
+      <GestureHandlerRootView>
+        <BottomSheetModalProvider>
+          <Stack
+            screenOptions={{
+              headerBackTitleVisible: false,
+              title: "",
+              headerBackButtonMenuEnabled: false,
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="create/entry" options={{ headerShown: true }} />
+            <Stack.Screen name="index" />
+            <Stack.Screen name="auth/signIn" />
+            <Stack.Screen name="auth/signUp" />
+            <Stack.Screen name="profile/[id]" options={{ headerShown: true }} />
+          </Stack>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
     </UserProvider>
   );
 };
